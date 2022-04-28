b7f9117ec0c9dbbd66997c25003da1902a939939
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-04-28 14:37:34 -0700

    commit tree 34677333e01748411696943abc561eaa6273223b

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,40 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 
-function ListingCard({ listing, img, description, location, onDeleteListing }) {
-  const [status, setStatus] = useState(false)
-  
-  function handleDeleteListingClick() {
-    fetch(`http://localhost:6001/listings/${listing.id}`, {
-      method: "DELETE"
-    }, [])
-    .then((res) => res.json())
-    .then(() => onDeleteListing(listing))
+function ListingCard({ listing, onDeleteListing }) {
+  const {id, description, image, location} = listing
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => onDeleteListing(listing))
   }
 
-  function handleStatusTrue() {
-    setStatus(true)
-  }
-
-  function handleStatusFalse() {
-    setStatus(false)
-  }
-
-
-   return (
+  return (
     <li className="card">
       <div className="image">
-        <span className="price">$0</span>
-        <img src={img} alt={description} />
+        <span className="price">0</span>
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {status ? (
-          <button onClick={handleStatusFalse} className="emoji-button favorite active">★</button>
+        {true ? (
+          <button className="emoji-button favorite active">★</button>
         ) : (
-          <button onClick={handleStatusTrue} className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite">☆</button>
         )}
         <strong>{description}</strong>
         <span> · {location}</span>
-        <button onClick={handleDeleteListingClick} className="emoji-button delete">🗑</button>
+        <button className="emoji-button delete" onClick={handleDeleteClick}>🗑</button>
       </div>
     </li>
   );
